cb8df941a07db5b08a9e6fd547dab4fa78dd7fc8
phungvanquy <phungvanquy97@gmail.com>
2022-11-18 16:48:04 +0300

    Edited on ubuntu

--- client/pub.py
+++ client/pub.py
@@ -1,15 +1,15 @@
 import paho.mqtt.client as mqtt
 import time
 
+
 # The callback for when the client receives a CONNACK response from the server.
+# Should not use time.sleep() in this call back
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    print(client)
+    if rc!=0 :
+        client.reconnect()
+        return
 
-
-    for i in range(1,5):
-        mess = client.publish("/phungvanquy/data/", 10)
-        client.loop_start()
     # Subscribing in on_connect() means that if we lose the connection and
 
     # reconnect then subscriptions will be renewed.
@@ -21,19 +21,13 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client()
-print(client)
 client.on_connect = on_connect
 client.on_message = on_message
 client.username_pw_set("quy", password="123456")
 
-client.connect("192.168.0.107", 1883, 60)
-
-
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_start()
-while True:
-    time.sleep(1)
+broker_addr = "localhost"
+broker_port = 1883
+keepAlive = 60
+client.connect(broker_addr, broker_port, keepAlive)
+client.publish("/phungvanquy/data/", None, qos = 0, retain=True)    # Retain True -> Whenever new user subscribes to topic, he will recieve the last previous payload. To Remove retained messg we just need to use payload=None and retain=True
+client.loop_forever()
--- client/sub.py
+++ client/sub.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import time
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -11,6 +12,7 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -18,6 +20,7 @@ client.on_message = on_message
 client.username_pw_set("quy", password="123456")
 
 client.connect("localhost", 1883, 60)
+
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
